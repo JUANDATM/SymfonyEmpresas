48fbd310f913d16142b3c6cd813f8168006f8a59
JUANDATM <kira00711@hotmail.com>
2020-01-08 15:31:24 -0600

    Acceso bundle , diseno, insercion

--- src/ControlAccesoBundle/Controller/DefaultController.php
+++ src/ControlAccesoBundle/Controller/DefaultController.php
@@ -3,11 +3,41 @@
 namespace ControlAccesoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use ControlAccesoBundle\Model\LoginModel;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    protected $UsuariosModel;
+
+    public function __construct() {
+        $this->LoginModel = new LoginModel();
+    }
+
+    public function loginUsuarioAction()
     {
-        return $this->render('ControlAccesoBundle:Default:index.html.twig');
+        return $this->render('ControlAccesoBundle:Acceso:loginUsuario.html.twig');
+    }
+
+    public function InsertarUsuarioAction(Request $request){   
+        $post = $request->request->all();
+
+        $data_Usuarios = array(
+            "NombreUsuario" => "'" . $post["nombre"] . "'",
+            "CorreoUsuario" =>"'". $post["correo"]."'",
+            "PasswordUsuario" =>"'". $post["password"]."'",
+            "DomicilioUsuario" => "'".$post["domicilio"]."'",
+            "TipoUsuario" => "'".$post["rol"]."'",
+        );
+        
+         $result_Usuarios = $this->LoginModel->insertarLoginUsuarios($data_Usuarios);
+         return $this->jsonResponse($result_Usuarios);
+
+    }
+    protected function jsonResponse($data) {
+        $response = new Response(json_encode($data));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 }
